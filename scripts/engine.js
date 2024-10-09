3f99beb53a0e943b7a79653b4575f9aeaed6b850
PBDylan <145608393+PBDylan@users.noreply.github.com>
2024-10-09 09:41:59 -0300

    Implementando funcionamento (listener + actions + functions)

--- scripts/engine.js
+++ scripts/engine.js
@@ -6,10 +6,27 @@ const state = {
         score: document.querySelector("#score"),
     },
     values: {
-        timerId: null,
         gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+    actions: {
+        timerId: null,
+        countDownTimerId: setInterval(countDown, 1000),
     },
 };
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if(state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over: O seu resultado foi: " + state.values.result);
+    }
+}
 
 function randomSquare() {
     state.view.square.forEach((square) => { // Correção: usar "square" corretamente
@@ -19,6 +36,7 @@ function randomSquare() {
     let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.square[randomNumber]; // Correção: usar "square" corretamente
     randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
 }
 
 function moveEnemy() {
@@ -27,12 +45,19 @@ function moveEnemy() {
 
 function addListenerHitbox() {
     state.view.square.forEach((square) => {
-    
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition){
+                state.values.result++
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }
+        })
     });
 }
 
 function init() {
-    randomSquare();
+    moveEnemy();
+    addListenerHitbox();
 }
 
 init();
